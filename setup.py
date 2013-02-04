9ab2f8dd8635010474f30da250ef48f301249b64
carlosmscabral <carlosmscabral@gmail.com>
2013-02-04 12:28:14 -0200

    Changed Makefile and setup.py

--- setup.py
+++ setup.py
@@ -10,22 +10,24 @@ import sys
 sys.path.append( '.' )
 from mininet.net import VERSION
 
-scripts = [ join( 'bin', filename ) for filename in [ 'mn' ] ]
+scripts = [ join( 'bin', filename ) for filename in [ 'mn' 'miniccnx' 'miniccnxedit' ] ]
 
 modname = distname = 'mininet'
 
 setup(
     name=distname,
     version=VERSION,
-    description='Process-based OpenFlow emulator',
-    author='Bob Lantz',
-    author_email='rlantz@cs.stanford.edu',
+    description='Process-based OpenFlow emulator with CCNx extension',
+    author='Bob Lantz, Carlos Cabral',
+    author_email='rlantz@cs.stanford.edu, cabral@dca.fee.unicamp.br',
     packages=find_packages(exclude='test'),
     long_description="""
         Mininet is a network emulator which uses lightweight
         virtualization to create virtual networks for rapid
         prototyping of Software-Defined Network (SDN) designs
-        using OpenFlow. http://openflow.org/mininet
+        using OpenFlow. http://openflow.org/mininet.
+	This also includes an extension for using Content Centric
+	Networks based on the NDN model (project CCNx).
         """,
     classifiers=[
           "License :: OSI Approved :: BSD License",
@@ -34,7 +36,7 @@ setup(
           "Intended Audience :: Developers",
           "Topic :: Internet",
     ],
-    keywords='networking emulator protocol Internet OpenFlow SDN',
+    keywords='networking emulator protocol Internet OpenFlow SDN CCNx CCN',
     license='BSD',
     install_requires=[
         'setuptools',
