b8b1806546eed12ba723625cabe83a528d66e5b1
Vince Lehman <vslehman@memphis.edu>
2015-07-14 13:07:22 -0500

    Add license and copyright notices
    Change-Id: I47244d7e3015c7d8a4c57f4ee1cc9679bc320d43

--- ndn/__init__.py
+++ ndn/__init__.py
@@ -1,1 +1,24 @@
+# -*- Mode:python; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
+#
+# Copyright (C) 2015 The University of Memphis,
+#                    Arizona Board of Regents,
+#                    Regents of the University of California.
+#
+# This file is part of Mini-NDN.
+# See AUTHORS.md for a complete list of Mini-NDN authors and contributors.
+#
+# Mini-NDN is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Mini-NDN is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Mini-NDN, e.g., in COPYING.md file.
+# If not, see <http://www.gnu.org/licenses/>.
+
 import experiment_manager as ExperimentManager
